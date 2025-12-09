e0ea5fdfb304616b1abf3cf9cd493542ce7d1506
Gabriele Balsamo <gabrielebalsamo2003@gmail.com>
2025-12-09 13:39:04 +0100

    feat: incomplete conversion implemented without argaparse

--- src/main.py
+++ src/main.py
@@ -14,30 +14,45 @@ def open_image(image_path: str):
         sys.exit(1)
 
 
-def convert_image_format(img: Image, source_path: str, format: str):
+def convert_image_format(img: Image, source_path: str, out_format: str):
+    filename_root = os.path.splitext(source_path)[0]
+    # filename_ext = os.path.splitext(source_path)[1].lstrip(".")
+    filename_output = f"{filename_root}.{out_format}"
+    print(f"Converto {source_path} in {filename_root}.{out_format}\n")
     try:
-        filename_root = os.path.splitext(source_path)[0]
-        filename_output = f"{filename_root}.{format}"
-
-        match format:
+        match out_format:
             case "jpg" | "jpeg":
-                ...
+                if img.mode in ("RGBA", "P"):
+                    img = img.convert("RGB")
+                img.save(filename_output, quality=90)
+                return True
             case "png":
-                ...
+                img.save(filename_output, optimize=True)
+                return True
             case "webp":
-                ...
-     
-    except Exception as e:
-        ...
+                img.save(filename_output)
+                return True
+            case _:
+                print("Formato non supportato\n")
+                return False
+    except (OSError, ValueError) as e:
+        print(f"Errore: {e}")
+
 
 def main():
+
     if len(sys.argv) < 2:
         print("Errore: Specifica il percorso dell'immagine.")
         sys.exit(1)
     image_path = sys.argv[1]
     img = open_image(image_path)
     if img:
-        ...
+        if not convert_image_format(
+            img, image_path, "jpg"
+        ):  # formato momentaneo, bisogna implementare flags con argparse
+            sys.exit(1)
+        else: 
+            print("Immagine convertita correttamente!")
 
 
 if __name__ == "__main__":
