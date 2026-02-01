15ddda736a6c4c968b647f8876435043be2c6e7c
Gabriele Balsamo <gabrielebalsamo2003@gmail.com>
2026-02-01 22:03:51 +0100

    test(to_ansi): tested to_ansi function

--- tests/helpers/ascii_ansi_helpers.py
+++ tests/helpers/ascii_ansi_helpers.py
@@ -1,6 +1,6 @@
-# tests/helpers_ascii.py
-from src.consts import ASCII_CHARS
+from src.consts import ASCII_CHARS, CHARACTER
 
+# ASCII HELPERS
 
 def ascii_for_luminance(l: int) -> str:
     n = len(ASCII_CHARS)
@@ -21,7 +21,7 @@ def pixel_to_ascii(pixel, colored=True):
         return char
 
 
-def build_expected(pixels, width, colored=True):
+def build_ascii_expected(pixels, width, colored=True):
     out = ""
     for i, pixel in enumerate(pixels):
         out += pixel_to_ascii(pixel, colored=colored)
@@ -33,3 +33,23 @@ def build_expected(pixels, width, colored=True):
 def rgba_to_la(pixels):
     """Converte una lista di pixel RGBA in LA (luminance, alpha)"""
     return [(int(0.2126 * r + 0.7152 * g + 0.0722 * b), a) for r, g, b, a in pixels]
+
+# ANSI HELPERS
+
+def pixel_to_ansi(pixel):
+    # pixel is (r,g,b,a)
+    r, g, b, a = pixel
+    if a == 0:
+        return "\033[0m "
+    else:
+        return f"\033[38;2;{r};{g};{b}m{CHARACTER}"
+
+
+def build_ansi_expected(pixels, width):
+    out = ""
+    for i, pixel in enumerate(pixels):
+        out += pixel_to_ansi(pixel)
+        if (i + 1) % width == 0:
+            out += "\033[0m\n"
+    out += "\033[0m"
+    return out
--- tests/test_convert/test_to_ascii.py
+++ tests/test_convert/test_to_ascii.py
@@ -1,71 +1,11 @@
-import pytest
 from PIL import Image
 from src.convert import to_ascii
-from tests.helpers.ascii_helpers import build_expected, rgba_to_la
+from tests.helpers.ascii_ansi_helpers import build_ascii_expected, rgba_to_la
 
 
-@pytest.fixture
-def mock_resize_img(mocker):
-    """Fixture to mock resize_img function"""
-    return mocker.patch("src.convert.resize_img")
-
-
-@pytest.fixture
-def img_width():
-    return 2
-
-
-global_pixels = pytest.mark.parametrize(
-    "pixels, colored, mode",
-    [
-        (
-            [
-                (255, 0, 0, 0),  # Transparent
-                (255, 0, 0, 255),  # Red
-                (0, 0, 255, 255),  # Blue
-                (0, 255, 0, 255),  # Green
-            ],
-            True,
-            "RGBA",
-        ),
-        (
-            [
-                (255, 0, 0, 255),  # Red
-                (0, 0, 255, 255),  # Blue
-                (0, 255, 0, 255),  # Green
-                (0, 10, 3, 0),  # Transparent
-            ],
-            False,
-            "LA",
-        ),
-        (  # Non-transparent pixels
-            [
-                (255, 0, 0, 255),
-                (255, 0, 0, 255),
-                (0, 0, 255, 255),
-                (0, 255, 0, 255),
-            ],
-            True,
-            "RGBA",
-        ),
-        (
-            [
-                (255, 0, 0, 255),
-                (0, 0, 255, 255),
-                (0, 255, 0, 255),
-                (0, 10, 3, 255),
-            ],
-            False,
-            "LA",
-        ),
-    ],
-)
-
-
-@global_pixels
-def test_to_ascii_colored(pixels, colored, mode, mock_resize_img, img_width, mocker):
-
-    expected = build_expected(pixels, img_width, colored)
+def test_to_ascii_colored(image_case, mock_resize_img, img_width, mocker):
+    pixels, colored, mode = image_case
+    expected = build_ascii_expected(pixels, img_width, colored)
 
     mock_converted_img = mocker.Mock(spec=Image.Image)
     if mode == "LA":
