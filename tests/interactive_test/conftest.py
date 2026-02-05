c9385bcbf358c02662af56f5f54fb4aa27ee42b4
Piero <fiorica.gianpiero@gmail.com>
2026-02-05 15:14:29 +0100

    refactor(tests/interactive_test)

--- tests/interactive_test/conftest.py
+++ tests/interactive_test/conftest.py
@@ -1,5 +1,18 @@
 import pytest
+from PIL import Image
+from unittest.mock import Mock
 
+@pytest.fixture
+def mock_img():
+    return Mock(spec=Image.Image)
+
+@pytest.fixture 
+def mock_generated_files():
+    return {
+        "jpg": ("/tmp/img.jpg",),
+        "png": ("/tmp/img.png",),
+        "webp": ("/tmp/img.webp",),
+    }
 
 @pytest.fixture()
 def mock_get_file_size(mocker):
--- tests/interactive_test/show_and_create_previews_test.py
+++ tests/interactive_test/show_and_create_previews_test.py
@@ -1,21 +1,18 @@
 import os
-from PIL import Image
-from unittest.mock import Mock
 from src.consts import ALL_FORMATS
 from src.interactive import show_and_create_previews
 
-img = Mock(spec=Image.Image)
 filename = os.path.basename("fake_test.py")
 output_dir = os.path.basename("./")
 
 
 def test_show_and_create_previews_full_success(
-    mock_convert_image, mock_get_file_size, capsys
+    mock_img, mock_convert_image, mock_get_file_size, capsys
 ):
 
     mock_get_file_size.return_value = "test KB"
     mock_convert_image.return_value = "mocked_ok"
-    result = show_and_create_previews(img, filename, output_dir)
+    result = show_and_create_previews(mock_img, filename, output_dir)
     expected_output = {
         fmt: (mock_convert_image.return_value, mock_get_file_size.return_value)
         for fmt in ALL_FORMATS
@@ -25,7 +22,7 @@ def test_show_and_create_previews_full_success(
 
 
 def test_show_and_create_previews_one_fail(
-    mock_convert_image, mock_get_file_size, capsys
+    mock_img,mock_convert_image, mock_get_file_size, capsys
 ):
 
     mock_get_file_size.return_value = "test KB"
@@ -40,15 +37,15 @@ def test_show_and_create_previews_one_fail(
         for i, fmt in enumerate(ALL_FORMATS[:-1])
     }
 
-    result = show_and_create_previews(img, filename, output_dir)
+    result = show_and_create_previews(mock_img, filename, output_dir)
 
     mock_convert_image.assert_called()
     assert expected_output == result
 
 
-def test_show_and_create_previews_fail(mock_convert_image, capsys):
+def test_show_and_create_previews_fail(mock_img,mock_convert_image, capsys):
     mock_convert_image.return_value = None
-    show_and_create_previews(img, filename, output_dir)
+    show_and_create_previews(mock_img, filename, output_dir)
 
     expected_output = []
 
